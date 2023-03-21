0ea38b2f1f4bc8eab667d6d539409296c1a4813d
amywandd <njeriamy2@gmail.com>
2023-03-21 01:21:17 -0700

    prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main- entry point.
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+putchar(95);
+putchar(112);
+putchar(117);
+putchar(116);
+putchar(99);
+putchar(104);
+putchar(97);
+putchar(114);
+return (0);
+}
