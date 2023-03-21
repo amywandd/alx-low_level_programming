a2b2e9cf50579d460d0d988832f9878c96f6476c
amywandd <njeriamy2@gmail.com>
2023-03-21 12:34:58 -0700

    prints alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,9 +3,10 @@
  * print_alphabet_x10 - prints the alphabet ten times
  * return: 0 (success)
  */
-
 void print_alphabet_x10(void)
-
+{
+int i = 0;
+while (i <= 10)
 {
 char c = 'a';
 while (c <= 'z')
@@ -14,4 +15,6 @@ putchar(c);
 c = c + 1;
 }
 putchar('\n');
+i = i + 1;
+}
 }
