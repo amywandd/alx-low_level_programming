d78eb6b23bc23261672f78495c4a597ccd1cc13b
amywandd <njeriamy2@gmail.com>
2023-03-22 05:22:17 -0700

    prints sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,15 +8,15 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-return "+";
+return +1;
 }
 else if (n == 0)
 {
-return "0";
+return (0);
 }
 else if (n < 0)
-{
-return "-";
+
+return -1;
 }
 return (0);
 }
