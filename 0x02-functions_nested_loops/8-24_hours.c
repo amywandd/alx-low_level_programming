0e7529e01c3d564e276fdeb721e50a6d6a84fec8
amywandd <njeriamy2@gmail.com>
2023-03-22 07:48:38 -0700

    prints every minute of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day
+ * Return: 0 success
+ */
+void jack_bauer(void)
+{
+int a, b;
+for (a = 0; a <= 23; a++)
+{
+for (b = 0; b <= 59; b++)
+{
+putchar (a / 10 + '0');
+putchar (a % 10 + '0');
+putchar (':');
+putchar (b / 10 + '0');
+putchar (b % 10 + '0');
+putchar ('\n');
+}
+}
+}
