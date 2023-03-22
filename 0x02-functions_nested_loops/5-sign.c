dea8ba61c52bb1f2d6f18b68a6e04d601aa0e804
amywandd <njeriamy2@gmail.com>
2023-03-22 04:25:46 -0700

    prints sign of number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,18 +8,15 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-putchar(43);
-return (1);
+return (43 + 1);
 }
 else if (n == 0)
 {
-putchar(48);
-return (0);
+return (48 + 0);
 }
 else if (n < 0)
 {
-putchar(45);
-return (-1);
+return (45 + 1);
 }
 return (0);
 }
