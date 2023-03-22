c9b342f38904e4fa37dcde0ac9104c93c10ec5ac
amywandd <njeriamy2@gmail.com>
2023-03-22 08:12:01 -0700

    adds two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - add two integers
+ * i: parameter function
+ * k: parameter function
+ * return: 0 (success)
+ */
+int add(int i, int k)
+{
+return (i + k);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,3 +10,4 @@ int print_last_digit(int c);
 void jack_bauer(void);
 void times_table(void);
 void print_to_98(int n);
+int add(int, int);
