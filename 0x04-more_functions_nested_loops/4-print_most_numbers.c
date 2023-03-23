321845e785418fcda3c9126349f0fff2daf010d6
amywandd <njeriamy2@gmail.com>
2023-03-23 09:33:55 -0700

    prints numbers 0 to 9 except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,14 +8,11 @@ void print_most_numbers(void)
 int c = '0';
 while (c <= '9')
 {
-if (c == '2' && c == '4')
+while (c == '2' && c == '4')
 {
 continue;
 }
-else
-{
 _putchar(c);
 c = c + 1;
-}
 }
 }
