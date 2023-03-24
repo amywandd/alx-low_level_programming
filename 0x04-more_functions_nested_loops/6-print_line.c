a6376452e5831319bae6e1d53c22c1eed1c219c7
amywandd <njeriamy2@gmail.com>
2023-03-24 03:15:19 -0700

    prints a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - prints a line
+ * Return: 0 (success)
+ */
+void print_line(int n)
+{
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+_putchar(95);
+_putchar('\n');
+}
+}
