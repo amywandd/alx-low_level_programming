6ee2f74f2722b3fcf8f98690baec4a46b098613d
amywandd <njeriamy2@gmail.com>
2023-03-27 09:57:11 -0700

    prints an output

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - function that prints a string
+ * @str: function parameter
+ * Return:0 (success)
+ */
+void _puts(char *str)
+{
+str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+puts(str);
+}
