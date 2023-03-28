b3a6d4f768a2fe018d3c99b6a58b69431bb73b58
amywandd <njeriamy2@gmail.com>
2023-03-28 05:39:34 -0700

    puts half the string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,16 +9,15 @@ void puts_half(char *str)
 {
 int n;
 int len = strlen(str);
-if (len % 2 == 0)
-{
-while ((n = len / 2))
-{
-_putchar(str[n]);
-n++;
-}
-}
-else
+if (len % 2 != 0)
 {
 n = (len - 1) / 2;
 }
+else
+{
+for (n = len / 2; n < len; n++)
+{
+}
+}
+_putchar('\n');
 }
