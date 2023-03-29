4bd84aa4d670aa7b33a447cdd57599d3e8517ec0
amywandd <njeriamy2@gmail.com>
2023-03-29 06:16:53 -0700

    strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strcat - concentrates two strings
+ * @dest: function parameter
+ * @src: function parameter
+ * Return: 0 (success)
+ */
+char *_strcat(char *dest, char *src)
+{
+strcat(src, dest);
+return (dest);
+}
