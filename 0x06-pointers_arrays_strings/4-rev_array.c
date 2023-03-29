bb94d0a2e53906e55dc6c6dd3ed2ad84f304bb07
amywandd <njeriamy2@gmail.com>
2023-03-29 09:46:57 -0700

    ar

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * reverse_array - reverses an array
+ * @a: parameter function
+ * @n: parameter function
+ * Return: 0 (success)
+ */
+void reverse_array(int *a, int n)
+{
+int i;
+for (i = n - 1; i >= 0; i--)
+{
+_putchar(a[i]);
+}
+}
