02cfec09ee35a90d4bfeeaaf368ef2c2ccda10f2
amywandd <njeriamy2@gmail.com>
2023-03-30 10:16:07 -0700

    cap_string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <ctype.h>
+#include <string.h>
+/**
+ * *cap_string - capitalises letters in a string
+ * @str: function parameter
+ * Return: 0(success)
+ */
+char *cap_string(char *str)
+{
+int i;
+int a = 1;
+while (str[i])
+{
+if (a && isalpha(str[i]))
+{
+str[i] = toupper(str[i]);
+a = 0;
+}
+else if (strchr(" \t\n,;.!?\"(){}", str[i]))
+{
+a = 1;
+}
+else
+{
+}
+i++;
+}
+return str;
+}
