56ec6ea0e25b466fca766cb010f93717c5376319
amywandd <njeriamy2@gmail.com>
2023-03-30 07:20:08 -0700

    reverse an array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * reverse_array - reverses an array
  * @a: parameter function
@@ -9,15 +8,10 @@
 void reverse_array(int *a, int n)
 {
 int i = n - 1;
-if (i == 0)
+while (i >= 0)
 {
-printf("\n");
+_putchar(a[i]);
+i++;
 }
-else
-{
-for (; i >= 0; i--)
-{
-printf("%d, ", a[i]);
-}
-}
+_putchar('\n');
 }
