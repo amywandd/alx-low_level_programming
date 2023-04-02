3df9c76dd75ec7a26057b87fa365b758fdcc1c33
amywandd <njeriamy2@gmail.com>
2023-04-02 11:20:49 -0700

    error

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,16 +10,15 @@ int main(void)
 int i;
 
 printf("Infinite loop incoming :(\n");
-
+/*
 i = 0;
-/* i has not been incremented */
+
 while (i < 10)
 {
 putchar(i);
 }
-
+*/
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
 }
-
