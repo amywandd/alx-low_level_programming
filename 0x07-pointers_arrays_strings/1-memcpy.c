f330ca81bda2d7e482ae4f384ffee120ddae7159
amywandd <njeriamy2@gmail.com>
2023-04-03 04:16:06 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_memcpy - copies a string
+ * @dest: parameter function
+ * @src: parameter function
+ * @n: parameter function
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+return (memcpy(dest, src, n));
+}
