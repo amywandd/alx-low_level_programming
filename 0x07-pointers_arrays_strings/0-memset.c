261090e226ac1bd7869805f5a499aef842ac8d22
amywandd <njeriamy2@gmail.com>
2023-04-03 04:07:54 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,8 +5,9 @@
  * @s: parameter function
  * @b: parameter function
  * @n: parameter function
+ * Return: s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-return memset(s, b, n);
+return (memset(s, b, n));
 }
