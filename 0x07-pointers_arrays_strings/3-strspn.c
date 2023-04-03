411ce1dc3df8407e08b7eb249dad3e060b8e64cb
amywandd <njeriamy2@gmail.com>
2023-04-03 04:35:18 -0700

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <string.h>
 /**
- * int _strspn - gets the length of a prefix substring
+ * _strspn - gets the length of a prefix substring
  * @s: parameter function
  * @accept: parameteer function
  * Return: s
