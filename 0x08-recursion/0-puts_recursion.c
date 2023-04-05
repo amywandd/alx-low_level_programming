7734dee7c389dae4f92f0511fd4791db9902e293
amywandd <njeriamy2@gmail.com>
2023-04-05 05:33:32 -0700

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts_recursion - function that returns a string
+ * @s: parameter function
+ * Return: 0 (success)
+ */
+void _puts_recursion(char *s)
+{
+puts(s);
+}
