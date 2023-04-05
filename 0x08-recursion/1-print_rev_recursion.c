f59e11e2f3111d72f3868888fb7215637ab78d43
amywandd <njeriamy2@gmail.com>
2023-04-05 08:42:55 -0700

    strlen

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: parameter function
+ * Return: 0(success)
+ */
+void _print_rev_recursion(char *s)
+{
+int i;
+int j;
+for (i = 0;
---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: parameter function
+ * Return: length of a string
+ */
+int _strlen_recursion(char *s)
+{
+return (strlen(s));
+}
