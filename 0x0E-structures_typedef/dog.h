c5631156f328c9fba6367bcbb283b34bedd87843
amywandd <njeriamy2@gmail.com>
2023-04-25 10:45:33 -0700

    structures

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct my_struct - Short description
+ * @a: First member
+ * @b: Second member
+ * @c: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
