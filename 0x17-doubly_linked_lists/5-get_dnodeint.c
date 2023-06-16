5632e31b2f89b3ce8029b6450df280bc9a2bd739
amywandd <njeriamy2@gmail.com>
2023-06-16 12:58:01 -0700

    gets the nth node of a double linked list

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -10,6 +10,8 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 dlistint_t *temp = head;
 unsigned int count = 0;
+if (head == NULL)
+return (NULL);
 while (temp != NULL)
 {
 temp = temp->next;
