c34d64c77207a10644899ce9cde4810fa5b0c923
amywandd <njeriamy2@gmail.com>
2025-08-27 10:34:44 -0700

    this file prints an output using c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-
+/*
+ * main - entry point
+ */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
