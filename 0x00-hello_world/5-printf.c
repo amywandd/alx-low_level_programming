6a72d2a0c4fbc5e93ca1f83203cd50f9c37448dc
amywandd <njeriamy2@gmail.com>
2025-08-27 10:43:06 -0700

    This file prints using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 /*
- * Main - entry pint
+ * Main - entry point
+ * Return - value success
  */
-int main(void){
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return(0);
+int main(void)
+{
+printf ("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
