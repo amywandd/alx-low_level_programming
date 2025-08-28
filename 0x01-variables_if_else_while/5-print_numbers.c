5d0570b443a5a7034fbd8004818b67fa7d18b7f7
amywandd <njeriamy2@gmail.com>
2025-08-28 09:35:31 -0700

    this file prints numbers from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+ * main - entry point
+ * return: value success
+ */
+int main(void)
+{
+int i = 0;
+for ( i = 0; i < 10; i++)
+{
+printf("%d", i);
+}
+printf("\n");
+return (0);
+}
